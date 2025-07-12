2b1cafa567266fef31af7cdacdc28cd52daefa04
adityaexe <kmradi2002@gmail.com>
2025-07-13 03:34:14 +0530

    feat: Add initial frontend structure with pages, components, and styles
    - Created main entry point with routing and context provider.
    - Implemented About, Cart, Collection, Contact, Home, Login, Orders, PlaceOrder, Product, and Verify pages.
    - Added global styles with Tailwind CSS and custom font imports.
    - Developed reusable components like Title, NewsletterBox, CartTotal, and ProductItem.
    - Integrated shopping cart functionality with context for state management.
    - Implemented payment methods and order verification with backend API.
    - Configured Vite for development and production builds.
    - Set up Vercel configuration for deployment.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            StitchMeUp is a clothing brand that specializes in custom-made
+            garments, offering a wide range of styles and fabrics to suit every
+            taste. Our mission is to provide high-quality, personalized clothing
+            that fits perfectly and reflects your unique style.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>contact@StitchMeUp.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ StitchMeUp- All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
